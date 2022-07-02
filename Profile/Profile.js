0bb5d55a1a4b1d71e7a8bee2c2fea8ab7d7cf2a6
Carl Meyer <carlmeyer1996@gmail.com>
2022-07-02 16:05:52 +0100

    Update Profile Screen
    - finish layout for profile screen

--- Profile/Profile.js
+++ Profile/Profile.js
@@ -1,11 +1,10 @@
 import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import WoofButton from "../shared/WoofButton";
 import AuthContext from "../Context/AuthContext";
 import Avatar from "../shared/Avatar";
 import Data from "../Home/Data";
 import Heading from "../shared/Heading";
-import Ionicons from "react-native-vector-icons/Ionicons";
 import ProfileItem from "./ProfileItem";
 
 const ProfileScreen = () => {
@@ -13,22 +12,42 @@ const ProfileScreen = () => {
   const profile = Data.woofs[0];
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <View style={styles.profileHeading}>
         <Avatar url={profile.avatar}></Avatar>
         <Heading>{profile.name}</Heading>
       </View>
       <View style={styles.profileDetails}>
-        <ProfileItem description="Date Of Birth" value={profile.dateOfBirth}></ProfileItem>
-        <ProfileItem description="Breed" value={profile.breed}></ProfileItem>
-        <ProfileItem description="Favorite Toy" value={profile.toy}></ProfileItem>
-        <ProfileItem description="CareTaker" value={profile.caretaker}></ProfileItem>
-        <ProfileItem description="Email" value={profile.email}></ProfileItem>
+        <ProfileItem
+          icon="calendar-outline"
+          description="Date Of Birth"
+          value={profile.dateOfBirth}
+        ></ProfileItem>
+        <ProfileItem
+          icon="paw-outline"
+          description="Breed"
+          value={profile.breed}
+        ></ProfileItem>
+        <ProfileItem
+          icon="tennisball-outline"
+          description="Favorite Toy"
+          value={profile.toy}
+        ></ProfileItem>
+        <ProfileItem
+          icon="person-outline"
+          description="CareTaker"
+          value={profile.caretaker}
+        ></ProfileItem>
+        <ProfileItem
+          icon="mail-outline"
+          description="Email"
+          value={profile.email}
+        ></ProfileItem>
       </View>
       <View style={styles.profileFooter}>
         <WoofButton text="Log out" onPress={signOut} />
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
@@ -48,25 +67,32 @@ const styles = StyleSheet.create({
     width: "100%",
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "red",
+    // backgroundColor: "red",
   },
   profileDetails: {
-    paddingHorizontal: 16,
-    backgroundColor: "yellow",
-    width: "100%"
+    // backgroundColor: "yellow",
+    width: "100%",
+    flex: 1,
+    justifyContent: "space-evenly",
   },
   profileItem: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
-    backgroundColor: "blue",
+    // backgroundColor: "blue",
     width: "100%",
   },
   profileFooter: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "green",
+    // backgroundColor: "green",
+    width: "100%",
+  },
+  toolbar: {
+    flexDirection: "row",
+    // backgroundColor: "grey",
+    justifyContent: "flex-end",
     width: "100%",
   },
 });
--- Profile/ProfileItem.js
+++ Profile/ProfileItem.js
@@ -2,14 +2,14 @@ import { StyleSheet, Text, View } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import * as React from "react";
 
-const ProfileItem = ({description, value}) => (
+const ProfileItem = ({ description, value, icon }) => (
   <View style={styles.profileItem}>
-    <View style={{flexDirection: "row", alignItems: "center", backgroundColor: "grey"}}>
-      <Ionicons name="paw" size={32}></Ionicons>
-      <Text style={{paddingHorizontal: 8}}>{description}</Text>
+    <View style={styles.ProfileItemDescription}>
+      <Ionicons name={icon} size={32}></Ionicons>
+      <Text style={{ paddingHorizontal: 16 }}>{description}:</Text>
     </View>
-    <View style={{backgroundColor: "orange"}}>
-      <Text style={{padding: 8}}>{value}</Text>
+    <View style={styles.ProfileItemValue}>
+      <Text>{value}</Text>
     </View>
   </View>
 );
@@ -18,9 +18,17 @@ const styles = StyleSheet.create({
   profileItem: {
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "space-between",
-    backgroundColor: "blue",
+    // backgroundColor: "blue",
     width: "100%",
+    paddingHorizontal: 16
+  },
+  ProfileItemDescription: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  ProfileItemValue: {
+    flex: 1,
   },
 });
 
