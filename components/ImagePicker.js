d3020c1fbc1e9307fefac6a040647027f24e3a67
Carl Meyer <carlmeyer1996@gmail.com>
2022-08-07 22:52:45 +0100

    Image Picker Styling

--- components/ImagePicker.js
+++ components/ImagePicker.js
@@ -1,6 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { Button, Image, View, Platform } from "react-native";
+import {
+  Button,
+  Image,
+  View,
+  StyleSheet,
+  Platform,
+  TouchableOpacity,
+  Text,
+} from "react-native";
 import * as ImagePicker from "expo-image-picker";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const WoofImagePicker = () => {
   const [image, setImage] = useState(null);
@@ -21,14 +30,39 @@ const WoofImagePicker = () => {
     }
   };
 
+  const clearImage = () => {
+    setImage(null);
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && (
-        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+    <TouchableOpacity style={styles.container} onPress={pickImage}>
+      {!image && (
+        <>
+          <Ionicons name="image-outline" size={32}></Ionicons>
+          <Text>Pick an image</Text>
+        </>
       )}
-    </View>
+
+      {image && (
+        <>
+          <Image source={{ uri: image }} style={styles.image} />
+          <Button title="Clear image" onPress={clearImage}></Button>
+        </>
+      )}
+    </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 20,
+  },
+});
 
 export default WoofImagePicker;
